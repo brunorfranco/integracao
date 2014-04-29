77ba5c555db62bc575aad7467a854ec73561489d
brunorfranco <brunozo_rf@hotmail.com>
2014-04-28 21:45:35 -0300

    Atualizacao de configuracoes
    Atualizacao de configuracoes

--- src/main/java/com/pliablematter/cloudstorage/Demo.java
+++ src/main/java/com/pliablematter/cloudstorage/Demo.java
@@ -6,15 +6,15 @@ public class Demo {
 
 	public static void main(String[] args) throws Exception {
 		
-//		CloudStorage.createBucket("my-bucket-test1");
+//		CloudStorage.createBucket("bucket-bruno");
 		
-		CloudStorage.uploadFile("my-bucket-test1", "C:\\Users\\Bruno\\Documents\\Faculdade\\Integracao\\testtext.txt");
+		CloudStorage.uploadFile("bucket-bruno", "C:\\Users\\Bruno\\Documents\\Faculdade\\Integracao\\testtext2.txt");
 		
-		CloudStorage.downloadFile("my-bucket-test1", "testtext.txt", "C:\\Users\\Bruno\\Documents\\Faculdade");
+		CloudStorage.downloadFile("bucket-bruno", "testtext2.txt", "C:\\Users\\Bruno\\Documents\\Faculdade");
 		
 		List<String> buckets = CloudStorage.listBuckets();
 		
-		List<String> files = CloudStorage.listBucket("my-bucket-test1");
+		List<String> files = CloudStorage.listBucket("bucket-bruno");
 
 	}
 }
