a319b5da4b8a91ac28728b0ea54eb81616811ce6
brunorfranco <brunozo_rf@hotmail.com>
2014-04-29 13:46:09 -0300

    Correcoes
    Correcao do metodo de download de arquivos do bucket.

---
+++ src/main/java/com/pliablematter/cloudstorage/CodigoExemplo.java
@@ -0,0 +1,20 @@
+package com.pliablematter.cloudstorage;
+
+import java.util.List;
+
+public class CodigoExemplo {
+
+	public static void main(String[] args) throws Exception {
+		
+//		GerenciadorDaNuvem.createBucket("bucket-bruno");
+		
+		GerenciadorDaNuvem.uploadArquivo("bucket-bruno", "C:\\Users\\Bruno\\Documents\\Faculdade\\Integracao\\ic_launcher-web.png");
+		
+		GerenciadorDaNuvem.downloadArquivo("bucket-bruno", "ic_launcher-web.png", "C:\\Users\\Bruno\\Documents\\Faculdade");
+		
+		List<String> buckets = GerenciadorDaNuvem.listBuckets();
+		
+		List<String> files = GerenciadorDaNuvem.listBucket("bucket-bruno");
+
+	}
+}
