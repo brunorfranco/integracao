58ba494c0e8b51e0feddeed7a29f1069f2ede54b
brunorfranco <brunozo_rf@hotmail.com>
2014-06-04 22:49:03 -0300

    Projeto Queue novo
    Iniciado um projeto com estrutura nova.

---
+++ projetoQueue2/projetoQueue2/src/com/queue2/server/CriadorMensagensServlet.java
@@ -0,0 +1,51 @@
+package com.queue2.server;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
+
+
+
+@SuppressWarnings("serial")
+public class CriadorMensagensServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+
+		String mensagemResposta = "";
+		resp.setContentType("text/plain");
+		try {
+			String mensagem = req.getParameter("msg");
+
+			if (mensagem == null)
+				throw new Exception("Mensagem vazia.");
+
+			mensagem = mensagem.trim();
+			if (mensagem.length() == 0)
+				throw new Exception("Mensagem vazia.");
+			
+			Queue queue = QueueFactory.getQueue("fila-mensagens");
+			queue.add(TaskOptions.Builder.withUrl("/consomemensagem").param(
+					"msg", mensagem));
+			
+			mensagemResposta = "Mensagem adicionada a fila.";
+			resp.getWriter().println(mensagemResposta);
+		} catch (Exception ex) {
+			mensagemResposta = "Falha: " + ex.getMessage();
+			resp.getWriter().println(mensagemResposta);
+		}
+	}
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
