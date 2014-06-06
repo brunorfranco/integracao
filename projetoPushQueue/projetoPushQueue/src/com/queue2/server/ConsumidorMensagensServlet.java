ed55e37a9ed63661cf2bdb0700af8b5f0d97cc31
brunorfranco <brunozo_rf@hotmail.com>
2014-06-06 14:01:03 -0300

    Criand pullqueue
    Organizando o pushqueue e criando o projeto pullqueue

---
+++ projetoPushQueue/projetoPushQueue/src/com/queue2/server/ConsumidorMensagensServlet.java
@@ -0,0 +1,52 @@
+package com.queue2.server;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskHandle;
+
+
+@SuppressWarnings("serial")
+public class ConsumidorMensagensServlet extends HttpServlet {
+	private static final Logger _logger = Logger
+			.getLogger(ConsumidorMensagensServlet.class.getName());
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+
+		String mensagemResposta = "";
+		resp.setContentType("text/plain");
+		try {
+			String mensagem = req.getParameter("msg");
+			_logger.info("Mensagem que chegou : "
+					+ mensagem);
+			//
+			// Coloco as regras de negócio aqui
+			//
+			
+			mensagemResposta = "SUCCESSO: Mensagem chegou com sucesso";
+			_logger.info(mensagemResposta);
+			resp.getWriter().println(mensagemResposta);
+		} catch (Exception ex) {
+			mensagemResposta = "Falha: " + ex.getMessage();
+			_logger.info(mensagemResposta);
+			resp.getWriter().println(mensagemResposta);
+		}
+	}
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+}
